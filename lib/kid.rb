18e79219a1aba631a8e152be9e0d770febf1d026
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-21 16:40:06 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -5,7 +5,7 @@ require_relative './fancy_dance.rb'
 class Kid
     # include Dance
     # extend MetaDancing
-    extend FancyDance:ClassMethods
+    extend FancyDance::ClassMethods
     include FancyDance::InstanceMethods
 
     attr_accessor :name
