d9fea29263b5db7da2c56109f08704e97fa51647
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-21 16:39:54 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -6,7 +6,7 @@ class Dancer
     # include Dance
     # extend MetaDancing
     extend FancyDance::ClassMethods
-    include FancyDance::InstanceMethods 
+    include FancyDance::InstanceMethods
 
     attr_accessor :name
 
