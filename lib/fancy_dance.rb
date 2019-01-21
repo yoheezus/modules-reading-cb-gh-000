64d887cd1e5b73443af785c0181047a6f5e59375
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-21 16:21:57 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -17,4 +17,11 @@ module FancyDance
           "Thank you, thank you. It was a pleasure to dance for you all."
         end
     end
+
+    module ClassMethods
+
+        def metadata
+            "This class produces objects that love to dance."
+        end
+    end
 end
